61b5f4cbba4f8e2754a82c17558d4247902a5be6
wcnnkh <247044007@qq.com>
2021-08-15 20:52:23 +0800

    cleanup

---
+++ taxi-example/src/test/java/io/github/wcnnnkh/QueryTest.java
@@ -0,0 +1,37 @@
+package io.github.wcnnnkh;
+
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.TermQuery;
+
+import scw.lucene.DefaultLuceneTemplete;
+import scw.lucene.LuceneTemplate;
+import scw.lucene.SearchParameters;
+
+public class QueryTest {
+	public static void main(String[] args) {
+		LuceneTemplate luceneTemplate = new DefaultLuceneTemplete("test");
+
+		TestBean bean1 = new TestBean();
+		bean1.setName("a");
+		bean1.setValue("sss");
+
+		TestBean bean2 = new TestBean();
+		bean2.setName("a");
+		bean2.setValue("adsfdsfsf");
+
+		luceneTemplate.saveOrUpdate(new Term("name", "a"), bean1);
+		luceneTemplate.saveOrUpdate(new Term("name", "a"), bean2);
+
+		luceneTemplate
+				.search(new SearchParameters(new TermQuery(
+						new Term("name", "a")), 10), (document) -> {
+					TestBean testBean = new TestBean();
+					testBean.setName(document.get("name"));
+					testBean.setValue(document.get("value"));
+					return testBean;
+
+				}).stream().forEach((b) -> {
+					System.out.println(b);
+				});
+	}
+}
